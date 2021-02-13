97cc1223a051f9d6ef3e428722a9afe4f9cfce23
Pedro H G Migacz <pedromigacz@alunos.utfpr.edu.br>
2021-02-13 19:02:40 -0300

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'd26ab350e18f1bc51297ec6a69ded158'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'c63d419d-65a2-45dc-b131-a83245edf213'
+};
