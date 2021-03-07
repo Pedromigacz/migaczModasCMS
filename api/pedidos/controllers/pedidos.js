dd49129b4d7f4b027c481f5f5bf60f910318ada5
Pedro H G Migacz <pedromigacz@alunos.utfpr.edu.br>
2021-03-07 16:51:05 -0300

    pre commit

--- api/pedidos/controllers/pedidos.js
+++ api/pedidos/controllers/pedidos.js
@@ -1,8 +1,21 @@
 'use strict';
 
-/**
- * Read the documentation (https://strapi.io/documentation/developer-docs/latest/concepts/controllers.html#core-controllers)
- * to customize this controller
- */
+const { parseMultipartData, sanitizeEntity } = require('strapi-utils');
 
-module.exports = {};
+module.exports = {
+    async create(ctx) {
+
+        if (ctx.is('multipart')) {
+            return {
+                status: 406,
+                message: "Multipart not accepted"
+            }
+        }
+
+        //let entity = await strapi.services.pedidos.create(ctx.request.body);
+        // return sanitizeEntity(entity, { model: strapi.models.pedidos });
+
+        console.log(ctx.request.body)
+        return 'DEU BOM'
+      },
+};
