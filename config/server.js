63d6d59641e44f6939ac2f7cff2fff01d2e4bb78
Pedro H G Migacz <pedromigacz@alunos.utfpr.edu.br>
2021-03-19 23:41:14 -0300

    final cms

--- config/server.js
+++ config/server.js
@@ -1,9 +1,11 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('URL', 'https://api.migaczmodas.com'),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', 'd26ab350e18f1bc51297ec6a69ded158'),
+      secret: env('ADMIN_JWT_SECRET', '4c104eb7060541a04d78dc1f29d60cf3'),
     },
+    autoOpen: false,
   },
 });
