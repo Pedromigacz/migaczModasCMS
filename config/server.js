176c70c5bd888b8c1ba75a877be759f2e4888b89
Pedro H G Migacz <pedromigacz@alunos.utfpr.edu.br>
2021-03-21 16:24:22 -0300

    fix admin panel

--- config/server.js
+++ config/server.js
@@ -1,7 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('URL', 'https://api.migaczmodas.com'),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '4c104eb7060541a04d78dc1f29d60cf3'),
